4b4312cb2d753b8a3604a194b9c72620c24f1ba8
Justin Gardner <justin@rhynorater.com>
2026-07-15 06:05:33 -0500

    2.5.2

--- email-encoder-bundle/vendor/composer/installed.php
+++ email-encoder-bundle/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'onlineoptimisation/email-encoder-bundle',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '839f4fcf74a0600483847074394d3d890b540d6f',
+        'reference' => '94da8b007cb2f329596bce6b7c5195261fd1468c',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'onlineoptimisation/email-encoder-bundle' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '839f4fcf74a0600483847074394d3d890b540d6f',
+            'reference' => '94da8b007cb2f329596bce6b7c5195261fd1468c',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
